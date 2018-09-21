75225f759e23fbe3b47fc064ede6140aeb5560db
nakulsai <nakulstar95@gmail.com>
2018-09-21 06:32:27 +0530

    mltut-2

--- basics.py
+++ basics.py
@@ -19,7 +19,7 @@ def check_even(k):
     except:
         print('its error')
 
-print(check_even('bhfh'))
+# print(check_even('bhfh'))
 
 def check_even_odd(k):
     if type(k) not in ['int','float']:
@@ -27,4 +27,20 @@ def check_even_odd(k):
     else:
         print(k)
 
-check_even_odd('98')
+# check_even_odd('98')
+
+
+user_age = 28
+print(user_age == str)
+
+sentence = 'this is class'
+sentence[0]
+sentence[-1]
+
+k = [1,2,3,'nakul']
+type(k)
+k[:3]
+k[0:3]
+k[1:3]
+k[-1]
+k[2:-1]
